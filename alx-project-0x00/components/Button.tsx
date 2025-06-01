bf9572f4d541136027aeb13c044fda60edb96714
Valentine Irungu <irunguvalentine73@gmail.com>
2025-06-01 20:52:20 +0300

    Updated ButtonProps interface-styles to string

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { ButtonProps } from '@/interfaces'
 
-const Button: React.FC<ButtonProps> = ({title, className=""}) => {
+const Button: React.FC<ButtonProps> = ({title, styles}) => {
   return (
-    <button className = {`px-4 py-2 font-semibold ${className}`}>{title}</button>
+    <button className = {`px-4 py-2 font-semibold ${styles}`}>{title}</button>
   )
 }
 
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -9,19 +9,19 @@ const Landing: React.FC = () => {
             <Card />
             <div className="flex flex-col gap-4 items-center mt-10">
                 {/* Small Buttons */}
-                <Button title="Small Rounded-sm" className="text-sm border rounded-sm" />
-                <Button title="Small Rounded-md" className="text-sm border rounded-md" />
-                <Button title="Small Rounded-full" className="text-sm border rounded-full" />
+                <Button title="Small Rounded-sm" styles="text-sm border rounded-sm" />
+                <Button title="Small Rounded-md" styles="text-sm border rounded-md" />
+                <Button title="Small Rounded-full" styles="text-sm border rounded-full" />
 
                 {/* Medium Buttons */}
-                <Button title="Medium Rounded-sm" className="text-base border rounded-sm" />
-                <Button title="Medium Rounded-md" className="text-base border rounded-md" />
-                <Button title="Medium Rounded-full" className="text-base border rounded-full" />
+                <Button title="Medium Rounded-sm" styles="text-base border rounded-sm" />
+                <Button title="Medium Rounded-md" styles="text-base border rounded-md" />
+                <Button title="Medium Rounded-full" styles="text-base border rounded-full" />
 
                 {/* Large Buttons */}
-                <Button title="Large Rounded-sm" className="text-lg border rounded-sm" />
-                <Button title="Large Rounded-md" className="text-lg border rounded-md" />
-                <Button title="Large Rounded-full" className="text-lg border rounded-full" />
+                <Button title="Large Rounded-sm" styles="text-lg border rounded-sm" />
+                <Button title="Large Rounded-md" styles="text-lg border rounded-md" />
+                <Button title="Large Rounded-full" styles="text-lg border rounded-full" />
             </div>
         </div>
     )
